b1a87a6ca68990627c1350213430ba311c936f42
virakesavan <virakesavan@gmail.com>
2025-10-27 10:59:27 +0530

    Updated homepage message for Windows version

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello Vira! This website is running on Windows 🚀')
 })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
