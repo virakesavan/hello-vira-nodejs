d914e5748a1080eb8a0226b8d9a5b0c53deaf008
virakesavan <virakesavan@gmail.com>
2025-10-27 12:35:43 +0530

    Added console log update by Vira

--- index.js
+++ index.js
@@ -11,4 +11,5 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+console.log("This is an updated version by Vira!");
 
